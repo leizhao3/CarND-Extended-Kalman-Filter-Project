9b112c5b96845a8d9761c61422bd806ac183b6e0
FN404NF <zl.stone1992@gmail.com>
2020-05-25 13:26:30 -0700

    Feat: initial project setup for EKF project. * Install WebSocket in the mac. Successfully run the simulator.

--- install-mac.sh
+++ install-mac.sh
@@ -1,15 +1,18 @@
 #! /bin/bash
 brew install openssl libuv cmake zlib
-git clone https://github.com/uWebSockets/uWebSockets 
+git clone https://github.com/uWebSockets/uWebSockets
 cd uWebSockets
 git checkout e94b6e1
 patch CMakeLists.txt < ../cmakepatch.txt
 mkdir build
-export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig 
+export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig
 cd build
 OPENSSL_VERSION=`brew list --versions openssl | cut -d' ' -f2`
+#! OPENSSL_VERSION="1.1.1d"
+brew install openssl libuv cmake
 cmake -DOPENSSL_ROOT_DIR=$(brew --cellar openssl)/$OPENSSL_VERSION -DOPENSSL_LIBRARIES=$(brew --cellar openssl)/$OPENSSL_VERSION/lib ..
-make 
+#! cmake -DOPENSSL_ROOT_DIR=$(brew --prefix openssl) -DOPENSSL_LIBRARIES=$(brew --prefix openssl)/lib ..
+make
 sudo make install
 cd ..
 cd ..
